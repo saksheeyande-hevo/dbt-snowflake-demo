954d4d3fce4c715b1ea59dc2299ea11f9107a713
saksheeyande-hevo <sakshee.yande@hevodata.com>
2026-02-27 17:30:01 +0530

    Update customers.sql

--- models/customers.sql
+++ models/customers.sql
@@ -1,6 +1,16 @@
 {{ config(
     materialized='table',
-    pre_hook="INSERT INTO DBT_AUDIT_LOG (MODEL_NAME, RUN_TIME) VALUES ('customers_model', CURRENT_TIMESTAMP)"
+
+    pre_hook="
+        INSERT INTO DBT_AUDIT_LOG (MODEL_NAME, RUN_TIME)
+        VALUES ('customers_pre', CURRENT_TIMESTAMP)
+    ",
+
+    post_hook="
+        INSERT INTO DBT_AUDIT_LOG (MODEL_NAME, RUN_TIME)
+        VALUES ('customers_post', CURRENT_TIMESTAMP)
+    "
 ) }}
 
-SELECT * FROM SAKSHEE_DB.PUBLIC.CUSTOMERS_RAW
+SELECT *
+FROM SAKSHEE_DB.PUBLIC.CUSTOMERS_RAW
